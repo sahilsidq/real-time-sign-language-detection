75cb6bba086c95e8244f637cc3af957af1df0c31
Sahil Siddique <sahilsidiquee@gmail.com>
2026-02-23 10:00:00 +0530

    adding abc file

---
+++ abc.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+import cv2
+import mediapipe as mp
+import cvzone
+import numpy as np
+
+print("TensorFlow:", tf.__version__)
+print("NumPy:", np.__version__)
+print("OpenCV:", cv2.__version__)
+print("Mediapipe:", mp.__version__)
+print("CVZone:", cvzone.__version__)
